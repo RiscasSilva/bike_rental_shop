958b2a773d1ae38a82cf8135de5c1e725cde8463
RiscasSilva <ricardoalexsilva121@gmail.com>
2020-09-14 12:42:00 +0100

    database

--- bike_rental.py
+++ bike_rental.py
@@ -6,67 +6,43 @@ class Shop:
         self.stock = stock
 
     def display_stock():
+
         print("We currently have " + self.stock + " available")
+
 
     def rent_H (self, n_bikes):
 
-        if int(n_bikes) <= 0:
-            print("Please pick a number greater than 0.")
-            return None
+        hour = datetime.datetime.now()
+        print("You have rented " + n_bikes + "bikes. You will be charged 5€ per hour. Starting rental hour: " + str(hour.hour))
+        self.stock -= int(n_bikes)
+        return hour
 
-        elif int(n_bikes) > self.stock:
-            print("We do not have that many bikes.")
-            return None
-
-        elif 0 < int(n_bikes) < self.stock:
-            hour = datetime.datetime.now()
-            print("You have rented " + n_bikes + "bikes. You will be charged 5€ per hour. Starting rental hour: " + str(hour.hour))
-            self.stock -= int(n_bikes)
-            return hour
 
     def rent_D (self, n_bikes):
 
-        if int(n_bikes) <= 0:
-            print("Please pick a number greater than 0.")
-            return None
+        hour = datetime.datetime.now()
+        print("You have rented " + n_bikes + "bikes. You will be charged 20€ per day. Starting rental hour: " + str(hour.hour))
+        self.stock -= int(n_bikes)
+        return hour
 
-        elif int(n_bikes) > self.stock:
-            print("We do not have that many bikes.")
-            return None
-
-        elif 0 < int(n_bikes) < self.stock:
-            hour = datetime.datetime.now()
-            print("You have rented " + n_bikes + "bikes. You will be charged 20€ per day. Starting rental hour: " + str(hour.hour))
-            self.stock -= int(n_bikes)
-            return hour
 
     def rent_W (self, n_bikes):
 
-        if int(n_bikes) <= 0:
-            print("Please pick a number greater than 0.")
-            return None
+        hour = datetime.datetime.now()
+        print("You have rented " + n_bikes + "bikes. You will be charged 60€ per week. Starting rental hour: " + str(hour.hour))
+        self.stock -= int(n_bikes)
+        return hour
 
-        elif int(n_bikes) > self.stock:
-            print("We do not have that many bikes.")
-            return None
-
-        elif 0 < int(n_bikes) < self.stock:
-            hour = datetime.datetime.now()
-            print("You have rented " + n_bikes + "bikes. You will be charged 60€ per week. Starting rental hour: " + str(hour.hour))
-            self.stock -= int(n_bikes)
-            return hour
 
     def return_bikes(self, rental_time, rental_basis, n_bikes):
-        
-        #Replenishes inventory
-        
+
+        #Replenishes inventory        
         bill = 0
         self.stock+=int(n_bikes)
 
         #bill calculation
-
         return_time = datetime.datetime.now()
-        rental_period = return_time - rental_time
+        rental_period = return_time - datetime.datetime.strptime(rental_time, '%Y-%m-%d %H:%M:%S.%f')
         if int(rental_basis) == 1:   # hourly rental
             bill = round(rental_period.seconds/3600) * 5 * int(n_bikes) + 5
         elif int(rental_basis) == 2:   # daily rental
@@ -77,6 +53,7 @@ class Shop:
         print("That would be: " + str(bill) + "€")
 
 
+
 class Costumer:
 
     def __init__ (self, rented_bikes=0, rental_time=0, rental_basis=0, bill=0):
@@ -85,17 +62,64 @@ class Costumer:
         self.rental_basis = rental_basis
         self.bill = bill
 
-    def rent_bikes(self):
+    def rent_bikes(self, stock):
         self.rented_bikes = 0
-        while int(self.rented_bikes) <= 0:
+        while int(self.rented_bikes) <= 0 or int(self.rented_bikes) > stock:
             self.rented_bikes = input("How many bikes do you want to rent? ")
+
+            if int(self.rented_bikes) <= 0:
+                print("Please pick a number greater than 0.")
+
+            elif int(self.rented_bikes) > stock:
+                print("We do not have that many bikes.")
 
         self.rental_basis = input("""   Request options:
 
-                1.  hourly basis
-                2.  daily basis
-                3.  weekly basis
+                1.  Hourly basis
+                2.  Daily basis
+                3.  Weekly basis
 
             Please select your option: """)
-        
 
+
+
+class Database:
+
+    def __init__(self):
+        self.id = []
+        self.name = []
+        self.rental_basis = []
+        self.rental_time = []
+        self.rented_bikes = []
+
+    def check_database(self, ID):
+        if ID in self.id:
+            return 0
+        else: 
+            return 1
+
+    def add(self, ID, costumer_name, rental_basis, rental_time, rented_bikes):
+        if ID in self.id:
+            return 0
+
+        else: 
+            self.id += ID
+            self.name += [costumer_name]
+            self.rental_basis += [rental_basis]
+            self.rental_time += [str(rental_time)]
+            self.rented_bikes += [rented_bikes]
+            return 1
+
+    def remove(self, ID):
+        if ID not in self.id:
+            return 0
+        
+        else:
+            self.name.remove(self.name[self.id.index(ID)])
+            self.rental_basis.remove(self.rental_basis[self.id.index(ID)])
+            self.rental_time.remove(self.rental_time[self.id.index(ID)])
+            self.rented_bikes.remove(self.rented_bikes[self.id.index(ID)])
+            self.id.remove(ID)
+            return 1
+
+    
--- bike_rental_main.py
+++ bike_rental_main.py
@@ -1,15 +1,19 @@
-from bike_rental import Shop, Costumer 
+from bike_rental import Shop, Costumer, Database
 
 shop = Shop(100)
-costumer = Costumer()
+database = Database()
+db_file = open("/home/cliente/Desktop/Python/bike_rental_shop/rental_database.txt", "w")
 
 while True:
 
-    action = input("Do you want to rent or to return bikes? ")
+    costumer = Costumer()
+
+    action = input("Hello! Do you want to rent or to return bikes? ")
+
 
     if action == "rent":
 
-        costumer.rent_bikes()
+        costumer.rent_bikes(shop.stock)
 
         if int(costumer.rental_basis) == 1:
             costumer.rental_time = shop.rent_H(costumer.rented_bikes)
@@ -23,6 +27,27 @@ while True:
             costumer.rental_time = shop.rent_W(costumer.rented_bikes)
             costumer.bill = 0
 
+        rental_id = input("Please write a three digits number. This will be your rental ID: ")
+        name = input("Please write your first and last name: ")
+
+        database.add(rental_id, name, costumer.rental_basis, costumer.rental_time, costumer.rented_bikes)
+        print(shop.stock)
+
+        del costumer
+
+
     if action == "return":
 
-        shop.return_bikes(costumer.rental_time, costumer.rental_basis, costumer.rented_bikes)
+        rental_id = input("What is your rental ID? ")
+
+        while rental_id not in database.id:
+            rental_id = input("That ID is not registered in our database. Try again: ")
+
+        shop.return_bikes(database.rental_time[database.id.index(rental_id)], database.rental_basis[database.id.index(rental_id)], database.rented_bikes[database.id.index(rental_id)])
+        print("We now have " + str(database.rented_bikes[database.id.index(rental_id)]) + " more bikes to rent. Total number of available bikes: " + str(shop.stock))
+        
+  
+        database.remove(rental_id)
+        print("Return value:" + str(database.remove(rental_id)))
+    
+        del costumer
